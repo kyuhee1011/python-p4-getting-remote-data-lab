0f2db3580d9189a97d6217a66c55097cdf13923b
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-08-29 23:28:11 -0400

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,22 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        loads_list =[]
+        loads = json.loads(self.get_response_body())
+        return loads
+    
+        # loads_list =[]
+        # loads=json.loads(self.get_response_body())
+        # for load in loads:
+        #     loads_list.append(load["name", "occupation"])
+
+        # return loads_list
+    
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
+
